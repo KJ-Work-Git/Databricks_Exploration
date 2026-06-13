3a1e1c66ed4f154e672e829b7424f0b3d6455fa0
Kieran Pritchard <kj_worker@proton.me>
2026-06-13 13:51:09 +0100

    Update Data_Ingestion.py

--- 01/Data_Ingestion.py
+++ 01/Data_Ingestion.py
@@ -1,1 +1,28 @@
+####################################################################################
+# Set python variables for catalog, schema, and volume names
+catalog_name = "dbacademy"
+schema_name = "ingesting_data"
+volume_name = "myfiles"
+####################################################################################
 
+####################################################################################
+# Create the catalog, schema, and volume if they don't exist already
+spark.sql(f"CREATE CATALOG IF NOT EXISTS {catalog_name}")
+spark.sql(f"CREATE SCHEMA IF NOT EXISTS {catalog_name}.{schema_name}")
+spark.sql(f"CREATE VOLUME IF NOT EXISTS {catalog_name}.{schema_name}.{volume_name}")
+####################################################################################
+
+####################################################################################
+# Creates a file called employees.csv in the specified catalog.schema.volume
+import pandas as pd
+data = [
+    ["1111", "Kristi", "USA", "Manager"],
+    ["2222", "Sophia", "Greece", "Developer"],
+    ["3333", "Peter", "USA", "Developer"],
+    ["4444", "Zebi", "Pakistan", "Administrator"]
+]
+columns = ["ID", "Firstname", "Country", "Role"]
+df = pd.DataFrame(data, columns=columns)
+file_path = f"/Volumes/{catalog_name}/{schema_name}/{volume_name}/employees.csv"
+df.to_csv(file_path, index=False)
+################################################################################
